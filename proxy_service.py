a60b6b6aa318dda0ad0bf3f0549753040b710132
zenadazurli <zenadazurli@libero.it>
2026-05-23 20:19:21 +0200

    Add files via upload

---
+++ proxy_service.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, Response
+import requests
+
+app = Flask(__name__)
+
+PROXY = "http://sazz16014w96:t3vz152mql23@resi.fusionproxy.net:13822"
+proxies = {"http": PROXY, "https": PROXY}
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path):
+    target_url = f"https://{path}" if path else "https://api.ipify.org"
+    
+    # Inoltra la richiesta a FusionProxy
+    resp = requests.request(
+        method=request.method,
+        url=target_url,
+        headers={k: v for k, v in request.headers if k != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        proxies=proxies,
+        allow_redirects=False
+    )
+    
+    # Restituisci la risposta
+    response = Response(resp.content, status=resp.status_code)
+    for k, v in resp.headers.items():
+        response.headers[k] = v
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
